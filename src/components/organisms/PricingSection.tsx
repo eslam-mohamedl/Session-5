88779d5a4dd4b8ec51bcd9d7b53cc284c367ff12
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-04-01 21:41:13 +0200

    fix(plog) : edite bg

--- src/components/organisms/PricingSection.tsx
+++ src/components/organisms/PricingSection.tsx
@@ -1,6 +1,6 @@
 export default function PricingSection() {
   return (
-    <div className="bg-indigo-400 text-white w-full p-8 flex justify-center items-center">
+    <div className="bg-black text-white w-full p-8 flex justify-center items-center">
       Pricing Section
     </div>
   );
