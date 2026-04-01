ffef2fa06916285100b1b653f5260d65744d3d0d
HebaElgohary <hebasamir535@gmail.com>
2026-04-01 21:38:05 +0200

    style(about): change about bg color

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='bg-red-200 text-xl flex justify-center items-center !p-5'>About</div>
+    <div className='bg-green-300 text-xl flex justify-center items-center !p-5'>About</div>
   )
 }
